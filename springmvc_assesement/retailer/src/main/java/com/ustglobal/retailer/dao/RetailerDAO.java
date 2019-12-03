9945b70b32ee72746ea6c2574ed7b897860c8f1e
priyanka423 <pnka774@gmail.com>
2019-12-03 22:41:16 +0530

    my codes

---
+++ springmvc_assesement/retailer/src/main/java/com/ustglobal/retailer/dao/RetailerDAO.java
@@ -0,0 +1,16 @@
+package com.ustglobal.retailer.dao;
+
+import com.ustglobal.retailer.dto.OrderBean;
+import com.ustglobal.retailer.dto.ProductBean;
+import com.ustglobal.retailer.dto.RetailerBean;
+
+public interface RetailerDAO {
+
+	public int createProfile(RetailerBean Bean);
+	public RetailerBean login(int rid,String password);
+	public ProductBean searchProduct(int pid);
+	public boolean updatePassword(int rid,String password);
+	public double sumAmount(int rid);
+	public OrderBean retrieveOrder(int rid);
+
+}
---
+++ springmvc_assesement/retailer/src/main/java/com/ustglobal/retailer/service/RetailerService.java
@@ -0,0 +1,18 @@
+package com.ustglobal.retailer.service;
+
+import com.ustglobal.retailer.dto.OrderBean;
+import com.ustglobal.retailer.dto.ProductBean;
+import com.ustglobal.retailer.dto.RetailerBean;
+
+public interface RetailerService {
+
+	public int createProfile(RetailerBean Bean);
+	public RetailerBean login(int rid,String password);
+	public ProductBean searchProduct(int pid);
+	public boolean updatePassword(int rid,String password);
+	public double sumAmount(int rid);
+	public OrderBean retrieveOrder(int rid);
+
+
+
+}
